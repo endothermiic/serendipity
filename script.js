1fb8e202b21b583d9ac515aa651147bd2ed4aa96
Christina Pizzonia <christina.pizzonia@mail.utoronto.ca>
2023-05-18 20:30:37 -0400

    new file:   .DS_Store 	new file:   firstPost.html 	new file:   images/.DS_Store 	new file:   images/painting.jpeg 	new file:   index.html 	new file:   script.js 	new file:   styles.css

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var recentPostsData = [
+        { title: "first post", description: "first post description.", link: "firstPost.html" },
+
+    ];
+
+    var maxNum = 4;
+
+    var recentPostsContainer = document.getElementById("recentPostsContainer");
+
+    // Display the most recent posts up to maxNum
+    for (var i = 0; i < maxNum && i < recentPostsData.length; i++) {
+        var post = recentPostsData[i];
+        var postElement = document.createElement("article");
+
+        var titleElement = document.createElement("h3");
+        titleElement.textContent = post.title;
+
+        var descriptionElement = document.createElement("p");
+        descriptionElement.textContent = post.description;
+
+        var readMoreLink = document.createElement("a");
+        readMoreLink.href = post.link;
+        readMoreLink.textContent = "Read More";
+
+        postElement.appendChild(titleElement);
+        postElement.appendChild(descriptionElement);
+        postElement.appendChild(readMoreLink);
+        recentPostsContainer.appendChild(postElement);
+    }
+
+    // Get the current year 
+    document.getElementById("year").textContent = new Date().getFullYear();
+});
